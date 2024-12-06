8d55eddbaa49d326e5a87f5e424573bec428fbc1
marc <marc.decker.public@gmail.com>
2024-12-06 20:57:25 +0100

    day5 impl

--- src/com/marc/aoc/Main.java
+++ src/com/marc/aoc/Main.java
@@ -5,8 +5,6 @@ import com.marc.aoc.day.*;
 public class Main {
 
     public static void main(String[] args) {
-        Day day = new Day05();
-        boolean testing = false;
-        DayRunner.run(day, testing);
+        DayRunner.run(new Day05(), false);
     }
 }
